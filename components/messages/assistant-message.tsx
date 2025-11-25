9a341e4ea72fcbe4e1ff1a41aed09168fa2f6c72
gilldarvesh <121057334+gilldarvesh@users.noreply.github.com>
2025-11-26 05:24:36 +0530

    Update assistant-message.tsx

--- components/messages/assistant-message.tsx
+++ components/messages/assistant-message.tsx
@@ -45,3 +45,6 @@ export function AssistantMessage({ message, status, isLastMessage, durations, on
                     }
                     return null;
                 })}
+            </div>
+        </div>
+    )
